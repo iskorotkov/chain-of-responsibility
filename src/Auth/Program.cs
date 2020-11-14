3a117a337428be8e57d4e0f937fff525d9afc310
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-11-14 18:33:10 +0500

    refactor(auth): refactor auth handlers and check user 3 times with different sets of handlers

--- src/Auth/Program.cs
+++ src/Auth/Program.cs
@@ -3,48 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using Auth;
 
-// Read from config files/env variables/command line arguments
-const bool useCache = true;
-const bool blockBruteForce = true;
-
-// Chain root
-var chain = new AuthHandler();
-
-// Add handlers
-if (useCache)
-{
-    chain.Add(new Cache());
-}
-
-chain.Add(new Sanitize());
-chain.Add(new Validate());
-if (blockBruteForce)
-{
-    chain.Add(new BlockBruteForce(maxAttempts: 3));
-}
-
-chain.Add(new Authenticate());
-chain.Add(new Authorize(role: "admin"));
-
-// Evaluate
-var user = new UserData("@iskorotkov", "ChainOfResponsibility");
-Console.WriteLine($"Athenticated and authorized: {chain.Handle(user)}");
+var user = new UserData("iskorotkov", "ChainOfResponsibility");
+var a1 = new Authenticator(useCache: false, blockBruteForce: false);
+Console.WriteLine($"Authenticated and authorized: {a1.Authenticate(user)}");
+var a2 = new Authenticator(useCache: false, blockBruteForce: true);
+Console.WriteLine($"Authenticated and authorized: {a2.Authenticate(user)}");
+var a3 = new Authenticator(useCache: true, blockBruteForce: true);
+Console.WriteLine($"Authenticated and authorized: {a3.Authenticate(user)}");
 
 namespace Auth
 {
     public sealed record UserData(string Username, string Password);
 
+    public class Authenticator
+    {
+        private readonly AuthHandler _handler;
+
+        public Authenticator(bool useCache, bool blockBruteForce)
+        {
+            // Chain root
+            _handler = new AuthHandler();
+
+            // Add handlers
+            if (useCache)
+            {
+                _handler.Add(new Cache());
+            }
+
+            _handler.Add(new Sanitize());
+            _handler.Add(new Validate());
+            if (blockBruteForce)
+            {
+                _handler.Add(new BlockBruteForce(maxAttempts: 3));
+            }
+
+            _handler.Add(new Authenticate());
+            _handler.Add(new Authorize(role: "editor"));
+        }
+
+        public bool Authenticate(UserData user) => _handler.Execute(user);
+    }
+
     public class AuthHandler
     {
         private AuthHandler? Next { get; set; }
 
-        public bool Handle(UserData user)
-        {
-            Console.WriteLine($"> Enter {ToString()}");
-            var result = Execute(user);
-            Console.WriteLine($"< Exit {ToString()}");
-            return result;
-        }
+        public virtual bool Execute(UserData user) => Next?.Execute(user) ?? true;
 
         public void Add(AuthHandler handler)
         {
@@ -57,31 +61,40 @@ namespace Auth
                 Next = handler;
             }
         }
-
-        protected bool ExecuteNext(UserData user) => Next?.Handle(user) ?? true;
-
-        protected virtual bool Execute(UserData user) => ExecuteNext(user);
     }
 
     public class Cache : AuthHandler
     {
-        protected override bool Execute(UserData user) => IsCached(user) || ExecuteNext(user);
+        public override bool Execute(UserData user)
+        {
+            Console.WriteLine(ToString());
+            return IsCached(user) || base.Execute(user);
+        }
+
         private bool IsCached(UserData user) => GetCache().Any(u => u == user.Username);
-        private static string[] GetCache() => new[] { "iskorotkov999" };
+        private static string[] GetCache() => new[] { "iskorotkov" };
     }
 
     public class Sanitize : AuthHandler
     {
-        protected override bool Execute(UserData user) => !user.Username.Any(IsDangerousSymbol) && ExecuteNext(user);
+        public override bool Execute(UserData user)
+        {
+            Console.WriteLine(ToString());
+            return !user.Username.Any(IsDangerousSymbol) && base.Execute(user);
+        }
+
         private bool IsDangerousSymbol(char c) => @"\@<>".Contains(c);
     }
 
     public class Validate : AuthHandler
     {
-        protected override bool Execute(UserData user) =>
-            !string.IsNullOrWhiteSpace(user.Username)
-            && !string.IsNullOrWhiteSpace(user.Password)
-            && ExecuteNext(user);
+        public override bool Execute(UserData user)
+        {
+            Console.WriteLine(ToString());
+            return !string.IsNullOrWhiteSpace(user.Username)
+                   && !string.IsNullOrWhiteSpace(user.Password)
+                   && base.Execute(user);
+        }
     }
 
     public class BlockBruteForce : AuthHandler
@@ -90,15 +103,23 @@ namespace Auth
 
         public BlockBruteForce(int maxAttempts) => _maxAttempts = maxAttempts;
 
-        protected override bool Execute(UserData user) =>
-            GetPreviousAttemptsFromDb(user) < _maxAttempts && ExecuteNext(user);
+        public override bool Execute(UserData user)
+        {
+            Console.WriteLine(ToString());
+            return GetPreviousAttemptsFromDb(user) < _maxAttempts && base.Execute(user);
+        }
 
-        private int GetPreviousAttemptsFromDb(UserData user) => 0;
+        private int GetPreviousAttemptsFromDb(UserData user) => 3;
     }
 
     public class Authenticate : AuthHandler
     {
-        protected override bool Execute(UserData user) => CheckCredentialsInDb(user) && ExecuteNext(user);
+        public override bool Execute(UserData user)
+        {
+            Console.WriteLine(ToString());
+            return CheckCredentialsInDb(user) && base.Execute(user);
+        }
+
         private bool CheckCredentialsInDb(UserData user) => true;
     }
 
@@ -107,7 +128,13 @@ namespace Auth
         private readonly string _role;
 
         public Authorize(string role) => _role = role;
-        protected override bool Execute(UserData user) => GetUserRolesFromDb(user).Contains(_role) && ExecuteNext(user);
+
+        public override bool Execute(UserData user)
+        {
+            Console.WriteLine(ToString());
+            return GetUserRolesFromDb(user).Contains(_role) && base.Execute(user);
+        }
+
         private IEnumerable<string> GetUserRolesFromDb(UserData user) => new[] { "editor" };
     }
 }
