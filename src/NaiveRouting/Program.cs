71c9f67fa77d766052b2919a89a25ce19f889658
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-11-18 20:59:39 +0500

    feat: add example with request routing

---
+++ src/NaiveRouting/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using NaiveRouting;
+
+// Pros:
+// - can add (remove, modify) handlers at runtime
+// - easy to write and use
+// - SRP
+// - OCP
+// Cons:
+// - handlers have no particular order
+
+var router = new Router<string>
+{
+    Routes =
+    {
+        ["/home"] = "Welcome to website",
+        ["/about"] = "Genius. Playboy. Philanthropist.",
+        ["*"] = "Error 404. Nothing here"
+    }
+};
+
+Console.WriteLine($"/home: {router.Route("/home")}");
+Console.WriteLine($"/about: {router.Route("/about")}");
+Console.WriteLine($"/pricing: {router.Route("/pricing")}");
+
+router.Routes.Add("/pricing", "Basic: $99.00, premium: $199.00");
+Console.WriteLine($"/pricing: {router.Route("/pricing")}");
+
+namespace NaiveRouting
+{
+    public class Router<T>
+    {
+        public Dictionary<string, T> Routes { get; } = new();
+
+        public T? Route(string request)
+        {
+            foreach (var (route, content) in Routes)
+            {
+                if (route == "*" || string.Equals(route, request, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return content;
+                }
+            }
+
+            return default;
+        }
+    }
+}
---
+++ src/Routing/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Routing;
+
+// Pros:
+// - can add (remove, modify) handlers at runtime
+// - easy to use for client
+// - SRP
+// - OCP
+// Cons:
+// - more code to handle request passing
+
+var router = new Rule<string>("/home", "Welcome to website",
+    new Rule<string>("/about", "Genius. Playboy. Philanthropist.",
+        new Rule<string>("*", "Error 404. Nothing here")));
+
+Console.WriteLine($"/home: {router.Route("/home")}");
+Console.WriteLine($"/about: {router.Route("/about")}");
+Console.WriteLine($"/pricing: {router.Route("/pricing")}");
+
+router = new Rule<string>("/pricing", "Basic: $99.00, premium: $199.00", router);
+Console.WriteLine($"/pricing: {router.Route("/pricing")}");
+
+namespace Routing
+{
+    public class Rule<T>
+    {
+        public Rule(string pattern, T content, Rule<T>? next = null)
+        {
+            Next = next;
+            Pattern = pattern;
+            Content = content;
+        }
+
+        public Rule<T>? Next { get; set; }
+        public string Pattern { get; set; }
+        public T Content { get; set; }
+
+        public T? Route(string request)
+        {
+            if (Pattern == "*" || string.Equals(request, Pattern, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return Content;
+            }
+
+            return Next != null ? Next.Route(request) : default;
+        }
+    }
+}
